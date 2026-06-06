d68aa248b9a18a92fef6c1b6c1948a71519b1fcb
Renzo7-programador7 <cordovarenzo737@gmail.com>
2026-06-06 10:49:12 -0500

    feat: agregar scripts de automatización gitflow

---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "=== Configurando proyecto gestion-tareas ==="
+git clone https://github.com/Renzo7-programador7/gestion-tareas.git
+cd gestion-tareas
+git checkout develop
+echo "✓ Proyecto listo"
